58919440866befc10124d1eb811d4e4516401913
OliveiraL21 <lucasoliveira508@gmail.com>
2025-07-17 12:09:52 -0300

    criando cadastro de empresa

--- src/app/pages/empresa/empresa-cadastro/empresa-cadastro.component.ts
+++ src/app/pages/empresa/empresa-cadastro/empresa-cadastro.component.ts
@@ -1,7 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { EmpresaCreate } from 'src/app/models/empresa/EmpresaCreate';
+import { EmpresaUpdate } from 'src/app/models/empresa/EmpresaUpdate';
+import { EmpresaService } from 'src/app/services/empresa/empresa.service';
 import { MensagemService } from 'src/app/services/message/Mensagem.service';
 
 @Component({
@@ -16,7 +20,7 @@ export class EmpresaCadastroComponent {
   title: string = 'Cadastro de Empresa';
   id: string = this.activetedRouter.snapshot.params['id'];
 
-  constructor(private messageService: MensagemService, private fb: FormBuilder, private router: Router, private activetedRouter: ActivatedRoute) { }
+  constructor(private messageService: MensagemService, private fb: FormBuilder, private router: Router, private activetedRouter: ActivatedRoute, private service: EmpresaService) { }
 
   initForm() {
     this.form = this.fb.group({
@@ -30,12 +34,58 @@ export class EmpresaCadastroComponent {
       bairro: [null, [Validators.required]],
       cep: [null, [Validators.required]],
       cidade: [null, [Validators.required]],
-      estado: [null, [Validators.required]],
+      estado: [null, [Validators.required, Validators.maxLength(2)]],
     });
   }
+
+  create(data: any) {
+    this.loading = true;
+    let empresa: EmpresaCreate = data;
+    empresa.cpf = empresa.cpf.replace(/\D/g, ''); // Remove non-numeric characters
+    this.service.create(empresa).subscribe({
+      next: (response: EmpresaCreate) => {
+        this.loading = false;
+        this.messageService.sucesso('Empresa', 'Empresa cadastrada com sucesso!');
+        this.router.navigateByUrl('empresa');
+      }, error: (error: HttpErrorResponse) => {
+        this.loading = false;
+      }
+    });
+  }
+
+  update(data: any) {
+    this.loading = true;
+    let empresa: EmpresaUpdate = data;
+    empresa.id = this.id;
+    empresa.cpf = empresa.cpf.replace(/\D/g, '');
+    this.service.update(this.id, empresa).subscribe({
+      next: (response: EmpresaUpdate) => {
+        this.loading = false;
+        this.messageService.sucesso('Empresa', 'Empresa atualizada com sucesso!');
+        this.router.navigateByUrl('empresa');
+      }, error: (error: HttpErrorResponse) => {
+        this.loading = false;
+      },
+    });
+  }
+
   save() {
-    if (this.form.invalid) {
+    if (this.form.valid) {
       console.log(this.form.value);
+      if (this.id) {
+        this.update(this.form.value);
+      } else {
+        this.create(this.form.value);
+      }
+    } else {
+      Object.values(this.form.controls).forEach((control: AbstractControl) => {
+        if (control.hasError('required') && control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity();
+        }
+      });
+      this.messageService.erro('Erro', 'Preencha todos os campos obrigatórios!');
+      this.loading = false;
     }
   }
   cancelar() {
