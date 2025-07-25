39c23e8b1d2bf4aa65cd904e8ab8e5283c551c5b
OliveiraL21 <lucasoliveira508@gmail.com>
2025-07-25 09:55:46 -0300

    orcamento hora cadastro

--- src/app/pages/orcamento-hora/orcamento-hora-cadastro/orcamento-hora-cadastro.component.ts
+++ src/app/pages/orcamento-hora/orcamento-hora-cadastro/orcamento-hora-cadastro.component.ts
@@ -1,14 +1,18 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormArray, FormArrayName, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConfirmationService } from 'primeng/api';
+import { Utils } from 'src/app/common/helpers/utils/utils';
 import Cliente from 'src/app/models/cliente/cliente';
 import { CustomFormControls } from 'src/app/models/custonsModels/CustomFormData/CustomFormControls';
 import CustomInputNumberData from 'src/app/models/custonsModels/customInputNumberData/CustomInputNumberData';
 import CustomSelectData from 'src/app/models/custonsModels/CustomSelect/CustomSelectData';
 import { Empresa } from 'src/app/models/empresa/Empresa';
+import { OrcamentoHoraCreate } from 'src/app/models/orcamentoHora/OrcamentoHoraCreate';
 import { ClienteService } from 'src/app/services/cliente-service/cliente.service';
 import { EmpresaService } from 'src/app/services/empresa/empresa.service';
+import { MensagemService } from 'src/app/services/message/Mensagem.service';
 import { OrcamentoHoraService } from 'src/app/services/orcamentoHora/orcamento-hora.service';
 
 @Component({
@@ -26,7 +30,7 @@ export class OrcamentoHoraCadastroComponent {
   empresas: Empresa[] = [];
 
 
-  constructor(private fb: FormBuilder, private service: OrcamentoHoraService, private clienteService: ClienteService, private empresaService: EmpresaService, private router: Router, private activatedRouter: ActivatedRoute, private confirmationService: ConfirmationService) { }
+  constructor(private fb: FormBuilder, private service: OrcamentoHoraService, private clienteService: ClienteService, private empresaService: EmpresaService, private router: Router, private activatedRouter: ActivatedRoute, private confirmationService: ConfirmationService, private messageService: MensagemService) { }
 
   getCustomFormControls(): CustomFormControls[] {
     return [
@@ -51,6 +55,26 @@ export class OrcamentoHoraCadastroComponent {
         data: new CustomInputNumberData('valorTotal', 'Valor Total', 'valorTotal', '', true, this.id ? true : false)
       },
     ]
+  }
+
+  getClientes() {
+    this.clienteService.listaSimples().subscribe({
+      next: (clientes: Cliente[]) => {
+        this.clientes = clientes;
+      }, error: (error: HttpErrorResponse) => {
+        this.loading = false;
+      }
+    })
+  }
+
+  getEmpresas() {
+    this.empresaService.list().subscribe({
+      next: (empresas: Empresa[]) => {
+        this.empresas = empresas;
+      }, error: (error: HttpErrorResponse) => {
+        this.loading = false;
+      }
+    })
   }
 
   initForm() {
@@ -99,8 +123,54 @@ export class OrcamentoHoraCadastroComponent {
 
   ngOnInit() {
     this.initForm();
+    this.getClientes();
+    this.getEmpresas();
     if (this.id) {
       this.title = 'Editar Orçamento';
     }
+  }
+
+  create(data: any) {
+    let orcamento: OrcamentoHoraCreate = {
+      empresaId: data.empresaId,
+      clienteId: data.clienteId,
+      valorHora: data.valorHora,
+      servicos: data.servicos.map((servico: any) => ({
+        descricao: servico.descricao,
+        quantidadeHora: servico.quantidadeHora,
+      }))
+    }
+    this.service.create(orcamento).subscribe({
+      next: (response: OrcamentoHoraCreate) => {
+        this.messageService.sucesso('Orçamento Por Hora', 'Orçamento criado com sucesso!');
+        this.loading = false;
+        this.router.navigateByUrl('orcamentoPorHora');
+      }, error: (error: HttpErrorResponse) => {
+        this.loading = false;
+      }
+    })
+  }
+
+  update(data: any) {
+
+  }
+  salvar() {
+    this.loading = true;
+    if (this.form.valid) {
+      let data = this.form.value;
+      if (!this.id) {
+        this.create(data);
+      } else {
+        this.update(data);
+      }
+    } else {
+      Utils.getRequiredFieldsInvalid(this.form);
+      this.messageService.erro('Error', 'Por favor preencha todos os campos obrigatórios.');
+      this.loading = false;
+    }
+  }
+
+  cancelar() {
+    this.router.navigateByUrl('orcamentoPorHora');
   }
 }
