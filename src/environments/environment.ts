cfe9373978a354e43af4fed111614185745cddc2
OliveiraL21 <lucasoliveira508@gmail.com>
2025-08-08 13:02:58 -0300

    alterando o enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   //URLs locales windows
-  api_url: 'https://localhost:44364',
-  api_usuario_url: 'https://localhost:44336',
+  api_url: 'https://localhost:5001',
+  api_usuario_url: 'https://localhost:6001',
 
 
   // api_url: 'http://192.168.0.19:83',
