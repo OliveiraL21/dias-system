5090d8ec9fa3b52bdc8b2531cab6eb54a52395d3
OliveiraL21 <lucasoliveira508@gmail.com>
2025-08-20 00:10:15 -0300

    alterando enviroment de producao para api no servidor local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https://localhost:44364',
-  api_usuario_url: 'https://localhost:44336',
+  api_url: 'https://192.168.0.105:443',
+  api_usuario_url: 'https://192.168.0.105:444',
 };
