aec8078ce3149fb30e7028baaca36b79561e188c
OliveiraL21 <lucasoliveira508@gmail.com>
2026-01-21 19:13:06 -0300

    ajuste de enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   //URLs locales windows
-  // api_url: 'https://localhost:5001',
-  // api_usuario_url: 'https://localhost:6001',
+  api_url: 'https://localhost:5001',
+  api_usuario_url: 'https://localhost:6001',
 
   // api_url: 'https://localhost:44364',
   // api_usuario_url: 'https://localhost:44336',
-  api_url: 'http://192.168.0.132:81',
-  api_usuario_url: 'http://192.168.0.132:82',
+  // api_url: 'http://192.168.0.132:81',
+  // api_usuario_url: 'http://192.168.0.132:82',
 };
 
 /*
