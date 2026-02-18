00a37c487fd239f62cc9dd3bc226dd1d23a82b46
OliveiraL21 <lucasoliveira508@gmail.com>
2026-02-18 12:55:58 -0300

    modificando os environments para auto ajuste

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  //URLs locales windows
+  // api_url: 'https://localhost:5001',
+  // api_usuario_url: 'https://localhost:6001',
+
+  api_url: 'https://localhost:44364',
+  api_usuario_url: 'https://localhost:44336',
+  // api_url: 'http://192.168.0.132:81',
+  // api_usuario_url: 'http://192.168.0.132:82',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  //URLs locales windows
-  api_url: 'https://localhost:5001',
-  api_usuario_url: 'https://localhost:6001',
-
-  // api_url: 'https://localhost:44364',
-  // api_usuario_url: 'https://localhost:44336',
-  // api_url: 'http://192.168.0.132:81',
-  // api_usuario_url: 'http://192.168.0.132:82',
+  production: true,
+  api_url: 'http://192.168.0.132:81',
+  api_usuario_url: 'http://192.168.0.132:82'
 };
 
 /*
