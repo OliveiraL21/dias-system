832e80c360545cbd3a8535a1ffa066853d8c7b73
OliveiraL21 <lucasoliveira508@gmail.com>
2026-03-11 07:36:58 -0300

    publicando versão nova

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: './src/assets/images/Logo dias system.png',
+    icon: 'src/assets/images/Logo dias system.png',
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
